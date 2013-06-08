04cd0686e7dfa044b7b145daf5c109e6f92fa22d
wolfgangihloff <wolfgang.ihloff@gmail.com>
2013-06-08 15:03:48 +0900

    add route for home path

--- app/routes.php
+++ app/routes.php
@@ -16,6 +16,11 @@ Route::get('/', function()
 	return View::make('home');
 });
 
+Route::get('home', function()
+{
+    return View::make('home');
+});
+
 Route::get('apps', function()
 {
     return View::make('apps');
