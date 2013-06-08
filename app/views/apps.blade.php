befe5d684775697a4543fa76b469c70aafb3f5d8
wolfgangihloff <wolfgang.ihloff@gmail.com>
2013-06-08 22:14:50 +0900

    updated apps and games

--- app/views/apps.blade.php
+++ app/views/apps.blade.php
@@ -1,12 +1,48 @@
 @extends('layout')
 
 @section('content')
-<div class="container text-left">
-<h1>Animal Quiz Game</h1>
-<p>Enter the name of the animal brand. <a href="games/">Start the quiz.</a></p>
 
-<h1>Animal Quiz Game</h1>
-<p>Enter the name of the animal. <a href="#">Start the quiz.</a></p>
+<br>
+<div class="container">
+    <div class="text-center">
+    <p class="lead">Welcome back. You have one unfinished assignment. Keep up the good work.</p>
+    <div class="row-fluid text-center">
+    <ul class="thumbnails example-sites">
+        <li class="span6">
+          <img class="img-polaroid" src="img/game2.png">
+          <p><b>Get to know the world.</b></p>
+          <p><i>Practice your knowledge of the countries of the world, browse around and learn about countries on all continents.</i></p>
+          <a href="#" class="btn btn-large btn-primary disabled">Completed</a>
+        </li>
+        <li class="span6">
+            <img src="img/game3.png" class="img-polaroid">
+            <p><b>Animal Quiz Game</b></p>
+            <p><i>Not all of these animals you have seen before or have you? Check if you can recognize different animals.</i></p>
+            <a href="games/" class="btn btn-large btn-primary">Start</a>
+        </li>
+    </ul>
+    </div>
+
+    <div class="row-fluid">
+    <ul class="thumbnails example-sites">
+        <li class="span6">
+           <img class="img-polaroid" src="img/game6.png">
+           <p><b>Space numbers</b></p>
+           <p><i>While flying through space, you need to stay alert to get all the space math right.</i></p>
+           <a href="#" class="btn btn-large btn-primary disabled">Completed</a>
+        </li>
+        <li class="span6">
+            <img class="img-polaroid" src="img/game7.png">
+            <p><b>Spelling bee.</b></p>
+            <p><i>Who is the next winner of spelling bee? More than 10000 words are randomly selected. Lead the high score.</i></p>
+            <a href="#" class="btn btn-large btn-primary disabled">Completed</a>
+        </li>
+    </ul>
+    </div>
 </div>
+</div>
+<br>
+<br>
+<br>
 @stop
 
--- app/views/games.blade.php
+++ app/views/games.blade.php
@@ -1,22 +1,52 @@
 @extends('layout')
 
 @section('content')
+<script>
+    onclick="$.cookie('clicks', $.cookie('clicks') + 1);"
+</script>
+
 <?php
 $solution = rand (1 ,19 );
-$fail = rand (1 ,19 );
-while($solution == $fail){
-    $fail= rand (1 ,19 );
+$fail1 = rand (1 ,19 );
+$fail2 = rand (1 ,19 );
+$fail3 = rand (1 ,19 );
+while($solution == $fail1 ){
+    $fail1= rand (1 ,19 );
+}
+while($solution == $fail2){
+    $fail2= rand (1 ,19 );
+}
+while($solution == $fail3){
+    $fail3= rand (1 ,19 );
 }
 
 $picture = DB::table('games')->where('id', $solution)->pluck('picture');
 $picture_word = DB::table('games')->where('id', $solution)->pluck('name');
-$picture_fail = DB::table('games')->where('id', $fail)->pluck('picture');
+$picture_fail1 = DB::table('games')->where('id', $fail1)->pluck('picture');
+$picture_fail2 = DB::table('games')->where('id', $fail2)->pluck('picture');
+$picture_fail3 = DB::table('games')->where('id', $fail3)->pluck('picture');
 ?>
 <div class="container text-center">
-<h1>Animal Quiz Game</h1>
-<p class="hint">Pick the image matching the word.</p><br>
-<p class="animal">{{ $picture_word }}</p>
-
-<p><a href="games/"><img src="{{ $picture }}"></a><a href="games/"><img src="{{ $picture_fail }}"></a></p>
+<h1>Animal Quiz</h1>
+    <br>
+    <br>
+<p class="hint">Pick the image matching the word <b>{{ $picture_word }}</b></p><br>
+<br>
+<br>
+<div>
+    <a href="games/">
+        <img class="img-polaroid" src="{{ $picture }}">
+    </a>
+    <a href="games/">
+        <img  class="img-polaroid" src="{{ $picture_fail1 }}">
+    </a>
+    <a href="games/">
+        <img  class="img-polaroid" src="{{ $picture_fail2 }}">
+    </a>
+    <a href="games/">
+        <img  class="img-polaroid" src="{{ $picture_fail3 }}">
+    </a>
 </div>
+</div>
+
 @stop
