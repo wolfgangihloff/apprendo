f2077b6d08a18b67a6b2730ea755c5d8daf0d294
wolfgangihloff <wolfgang.ihloff@gmail.com>
2013-06-08 23:14:21 +0900

    layout updated a little bit

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,30 +1,30 @@
 @extends('layout')
 
 @section('content')
-<h1>Login</h1>
 
-<!-- check for login error flash var -->
-@if (Session::has('flash_error'))
-<div id="flash_error">{{ Session::get('flash_error') }}</div>
-@endif
+<div class="container">
+    <div class="text-center">
+        <h1>Login</h1>
 
+        {{ Form::open(array('login', 'POST')) }}
 
-{{ Form::open(array('login', 'POST')) }}
+        <!-- username field -->
+        <p>
+            {{ Form::label('username', 'Username') }}
+            {{ Form::text('username', Input::old('username')) }}
+        </p>
 
-<!-- username field -->
-<p>
-    {{ Form::label('username', 'Username') }}<br/>
-    {{ Form::text('username', Input::old('username')) }}
-</p>
+        <!-- password field -->
+        <p>
+            {{ Form::label('password', 'Password') }}
+            {{ Form::password('password') }}
+        </p>
 
-<!-- password field -->
-<p>
-    {{ Form::label('password', 'Password') }}<br/>
-    {{ Form::password('password') }}
-</p>
+        <!-- submit button -->
+        <p>{{ Form::submit('Login') }}</p>
 
-<!-- submit button -->
-<p>{{ Form::submit('Login') }}</p>
+        {{ Form::close() }}
+    </div>
+</div>
 
-{{ Form::close() }}
 @stop
