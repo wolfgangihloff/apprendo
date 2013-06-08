346ca257443b389c5a3d7690e980ff28d7f999ed
Jo Jang <jangbaksa@gmail.com>
2013-06-07 22:34:57 -0700

    bootstrap edit

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,8 +1,165 @@
 @extends('layout')
 
 @section('content')
+	
+<!-- Hero Unit -->
+<div class="hero-unit">
+  <div class="container" align="center">
+    <h2 style="color:#595959">Apprendo.</h2>
+    <h4><p>E-learning contents marketplace with analytics</p></h4>
+      <form class="form-search">
+  <div class="input-append">
+    <input type="text" id="input"  class="span8" placeholder="e.g. Mathematics games, Physics quiz, Biology, Chemistry" style="background: #fff; border: 3px solid #00B300; height:40px; font-size:25px" autocomplete="off">
+  </div>
+</form>
 
-    <h1>Apprendo. All you want.</h1>
-    <a href="apps/">apps</a>
+<p><img src="img/trend.png" width="25" height="25" /> Trending: <span id="suggest"></span>English pics game, Maths quiz</p> 
+
+<p>Interactive e-learning marketplace with analytics to track performance.</p>
+    <a href="#" class="btn btn-large btn-success">Students</a>
+    <a href="../howitworks.php" class="btn btn-large btn-warning">Teachers</a>
+  </div>
+</div><div class="container">
+        <div class="marketing">
+            <h1>Introducing Apprendo for teachers</h1>
+            <p class="marketing-byline">This is basically how it works in 3 simple steps.</p>
+          <div class="row-fluid" align="center">
+            <div class="span4">
+              <img class="marketing-img" src="../img/1366692755_search.png" width="128" height="128" />
+              <h2>1. Teachers blablabla</h2>
+              BLABLBALBALBALSBDSLDNASLDKSALdsa asdfa sfasdfasdfasd
+            </div>
+            <div class="span4">
+              <img class="marketing-img" src="../img/1366692781_mail_receive.png" width="128" height="128" />
+              <h2>2. Teachers share knowledge</h2>
+            asdfasdlfjasd;lfkjas;dlfkja;sldfkja;sldkfjasd;lj.</div>
+            <div class="span4">
+              <img class="marketing-img" src="../img/1366692740_user_manage.png" width="128" height="128" />
+              <h2>3. Track class performance</h2>
+              asdfjasdhflkjah asdfalskdjfhalsd lkjhqewrlkjhasdlkjh</div>
+          </div>
+          
+<div class="container">
+        <div class="marketing">
+            <h1>For Students</h1>
+            <p class="marketing-byline">This is basically how it works in 3 simple steps.</p>
+          <div class="row-fluid" align="center">
+            <div class="span4">
+              <img class="marketing-img" src="../img/1366692755_search.png" width="128" height="128" />
+              <h2>1. Student learn interactively</h2>
+              BLABLBALBALBALSBDSLDNASLDKSALdsa asdfa sfasdfasdfasd
+            </div>
+            <div class="span4">
+              <img class="marketing-img" src="../img/1366692781_mail_receive.png" width="128" height="128" />
+              <h2>2. blablabla</h2>
+            asdfasdlfjasd;lfkjas;dlfkja;sldfkja;sldkfjasd;lj.</div>
+            <div class="span4">
+              <img class="marketing-img" src="../img/1366692740_user_manage.png" width="128" height="128" />
+              <h2>3. Track your performance</h2>
+              asdfjasdhflkjah asdfalskdjfhalsd lkjhqewrlkjhasdlkjh</div>
+          </div>
+        <br />
+        <p align="center">For detailed info <a href="#">click here.</a></p>
+
+<hr class="soften">
+
+
+<h1>Featured Apps</h1>
+    <p class="marketing-byline">Wanna share your quiz, test, games??, <a href="http://builtwithbootstrap.tumblr.com/" target="_blank">Share your stuff here</a> or <a href="./getting-started.html#examples">browse what others have shared</a></p>
+    
+	<div class="row-fluid">
+      <ul class="thumbnails example-sites">
+        <li class="span3">
+          <a class="thumbnail" href="http://soundready.fm/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/soundready.png" alt="SoundReady.fm">
+          </a>
+        <a href="#">Whatever game</a></li>
+        <li class="span3">
+          <a class="thumbnail" href="http://kippt.com/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/kippt.png" alt="Kippt">
+          </a>
+        <a href="#">Whatever quiz</a></li>
+        <li class="span3">
+          <a class="thumbnail" href="http://www.gathercontent.com/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/gathercontent.png" alt="Gather Content">
+          </a>
+          <a href="#">Whatever game</a>
+        </li>
+        <li class="span3">
+          <a class="thumbnail" href="http://www.jshint.com/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/jshint.png" alt="JS Hint">
+          </a>
+          <a href="#">Whatever game</a>
+        </li>
+      </ul>
+     </div>
+     <div class="row-fluid">
+      <ul class="thumbnails example-sites">
+        <li class="span3">
+          <a class="thumbnail" href="http://soundready.fm/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/soundready.png" alt="SoundReady.fm">
+          </a>
+        contact</li>
+        <li class="span3">
+          <a class="thumbnail" href="http://kippt.com/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/kippt.png" alt="Kippt">
+          </a>
+        </li>
+        <li class="span3">
+          <a class="thumbnail" href="http://www.gathercontent.com/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/gathercontent.png" alt="Gather Content">
+          </a>
+        </li>
+        <li class="span3">
+          <a class="thumbnail" href="http://www.jshint.com/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/jshint.png" alt="JS Hint">
+          </a>
+        </li>
+      </ul>
+     </div>
+     <div class="row-fluid">
+      <ul class="thumbnails example-sites">
+        <li class="span3">
+          <a class="thumbnail" href="http://soundready.fm/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/soundready.png" alt="SoundReady.fm">
+          </a>
+        contact</li>
+        <li class="span3">
+          <a class="thumbnail" href="http://kippt.com/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/kippt.png" alt="Kippt">
+          </a>
+        </li>
+        <li class="span3">
+          <a class="thumbnail" href="http://www.gathercontent.com/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/gathercontent.png" alt="Gather Content">
+          </a>
+        </li>
+        <li class="span3">
+          <a class="thumbnail" href="http://www.jshint.com/" target="_blank">
+          <img src="http://twitter.github.io/bootstrap/assets/img/example-sites/jshint.png" alt="JS Hint">
+          </a>
+        </li>
+      </ul>
+     </div>
+
+  </div>
+</div>
+<!-- End Hero Unit -->
+
+<!-- Modal -->
+<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="myModalLabel">Sign in</h3>
+  </div>
+  <div class="modal-body">
+    <p>Sign in</p>
+  </div>
+  <div class="modal-footer">
+    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+    <button class="btn btn-primary">Sign in</button>
+  </div>
+</div>
+<!--end Modal-->
 
 @stop
--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,49 +1,44 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="UTF-8">
-    <title>Apprendo</title>
-    <style>
-        @import url(http://fonts.googleapis.com/css?family=Lato:300,400,700);
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        body {
-            margin:0;
-            font-family:'Lato', sans-serif;
-            text-align:center;
-            color: #999;
-        }
+    <title>Apprendo - Fun and Interactive e-learning marketplace</title>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet" type="text/css" />	
+    <link href="bootstrap/css/styles.css" rel="stylesheet" type="text/css" />
 
-        .welcome {
-            width: 300px;
-            height: 300px;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            margin-left: -150px;
-            margin-top: -150px;
-        }
-
-        a, a:visited {
-            color:#FF5949;
-            text-decoration:none;
-        }
-
-        a:hover {
-            text-decoration:underline;
-        }
-
-        ul li {
-            display:inline;
-            margin:0 1.2em;
-        }
-
-        p {
-            margin:2em 0;
-            color:#555;
-        }
-    </style>
 </head>
 <body>
+<div class="navbar transparent navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+      <a class="brand" href="home/"><img src="img/logo_long_web.png" /></a>
+      <div class="nav-collapse">
+        <ul class="nav ">
+          <li><a href="home/"> Home</a></li>
+          <li><a href="students/">Students</a></li>
+          <li><a href="teachers/">Teachers</a></li>
+          <li><a href="help/">How it works</a></li>
+          <li><a href="pricing/">Pricing</a></li>
+        </ul>
+        <ul class="nav pull-right">
+          <li><a href="#myModal" data-toggle="modal"><i class="icon-home icon-white"></i> Sign in</a></li>
+        </ul>
+      </div><!-- /.nav-collapse -->
+    </div><!-- /.container -->
+  </div><!-- /.navbar-inner -->
+</div><!-- /.navbar -->
+
 @yield('content')
+
+<script src="bootstrap/js/bootstrap.js"></script>
+
 </body>
 </html>
