763bcc20b444763ee8e5d0ec0761cc47c840e8e9
Jo Jang <jangbaksa@gmail.com>
2013-06-08 05:02:50 -0700

    analytics updated

--- app/views/analytics.blade.php
+++ app/views/analytics.blade.php
@@ -26,16 +26,129 @@
      	<div class="span6">
             <div class="marketing">
                     <h2>Praise these students</h2>
+                 <div class="row">
+                    <div class="span3">
+                    Luis
+                    </div>
+                    <div class="span3">
+                    Score: 98
+                    </div>
+                 </div>
+                 <div class="row">
+                    <div class="span3">
+                    Pepe
+                    </div>
+                    <div class="span3">
+                    Score: 99
+                    </div>
+                 </div>
+                 <div class="row">
+                    <div class="span3">
+                    Ramon
+                    </div>
+                    <div class="span3">
+                    Score: 97
+                    </div>
+                 </div>
+                    
             </div>
        	</div>
         <div class="span6">
             <div class="marketing">
                     <h2>Tutor these students</h2>
+                    <div class="row">
+                        <div class="span3">
+                        Laura
+                        </div>
+                        <div class="span3">
+                        Score: 25
+                        </div>
+                 	</div>
+                    <div class="row">
+                        <div class="span3">
+                        Jose
+                        </div>
+                        <div class="span3">
+                        Score: 36
+                        </div>
+                 	</div>
+                    <div class="row">
+                        <div class="span3">
+                        Pepe
+                        </div>
+                        <div class="span3">
+                        Score: 28
+                        </div>
+                 	</div>
+                 
             </div>
         </div>
-     
+        
+        
+        <!--
+        <div class="row">
+        	<div class="marketing">
+                <div class="span6">
+                    Average no. of mistakes: 58
+                    <br/>
+                    Average completion time: 58 minutes
+                </div>
+                <div class="span6">
+                    Most common mistakes: lion, pig, shark, toad
+                </div>
+             </div>
+        </div>
+     -->
      </div>
+     
+     <hr>
+     
+     <div class="row">
+     	<div class="span6">
+            <div class="marketing">
+                 <div class="row">
+                    <div class="span3">
+                    Average no. of mistakes:
+                    </div>
+                    <div class="span3">
+                    58
+                    </div>
+                 </div>
+                 <div class="row">
+                    <div class="span3">
+                    Average completion time:
+                    </div>
+                    <div class="span3">
+                    25 minutes
+                    </div>
+                 </div>
+            </div>
+       	</div>
+        <div class="span6">
+            <div class="marketing">
+                    <div class="row">
+                        <div class="span3">
+                        Most common mistakes:</div>
+                        <div class="span3">
+                         lion, pig, shark, toad
+                        </div>
+                 	</div>
+                 
+            </div>
+        </div>
 </div>
+
+<hr>
+
+<div class="row">
+
+</div>
+
+<br/>
+<br/>
+<br/>
+<br/>
+
 
 <script>
 
