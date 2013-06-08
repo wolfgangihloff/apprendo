12f00a6c710da136c999a5a22be99a6188892f58
wolfgangihloff <wolfgang.ihloff@gmail.com>
2013-06-08 20:57:12 +0900

    clean games.blade.php

--- app/views/games.blade.php
+++ app/views/games.blade.php
@@ -1,16 +1,18 @@
 @extends('layout')
 
 @section('content')
-<h1>Animal Quiz Game</h1>
-
 <?php
 $solution = rand (1 ,19 );
-$fail = rand (1 ,20 );
+$fail = rand (1 ,19 );
 $fail++;
 
 $picture = DB::table('games')->where('id', $solution)->pluck('picture');
 $picture_word = DB::table('games')->where('id', $solution)->pluck('name');
-$picture_fail = DB::table('games')->where('id', $fail)->pluck('picture'); ?>
+$picture_fail = DB::table('games')->where('id', $fail)->pluck('picture');
+?>
+
+<h1>Animal Quiz Game</h1>
+
 <p>{{ $picture_word }}</p>
 <p><a href="games/"><img src="{{ $picture }}"></a><a href="games/"><img src="{{ $picture_fail }}"></a></p>
 
